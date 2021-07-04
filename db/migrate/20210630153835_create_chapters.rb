9be5eaff8986dd34a376992173d19d2ad5402ecd
khanhpn <phamkhanh90@gmail.com>
2021-07-04 13:45:59 +0700

    handle create crawler to get all data from beeng.org

--- db/migrate/20210630153835_create_chapters.rb
+++ db/migrate/20210630153835_create_chapters.rb
@@ -3,6 +3,7 @@ class CreateChapters < ActiveRecord::Migration[6.1]
     create_table :chapters do |t|
       t.string :title
       t.integer :view_counts
+      t.text :image_list
       t.belongs_to :product
 
       t.timestamps
--- lib/tasks/sites/beeng.rb
+++ lib/tasks/sites/beeng.rb
@@ -43,15 +43,15 @@ class Beeng
   # start using link category to get all products
   def collect_product_paging(category)
     get_total_paging = using_nokogiri(category[:category_link]).css("div.paging ul li.hidden-xs").first.content.to_i || 1
-    Category.find_or_create_by(name: category[:category_name])
+    current_category = Category.find_or_create_by(name: category[:category_name])
 
     (1..get_total_paging).each do |page|
-      get_product_links(("#{category[:category_link]}?page=#{page}"))
+      get_product_links(("#{category[:category_link]}?page=#{page}"), current_category)
     end
   end
 
   # collect all product
-  def get_product_links(product_url)
+  def get_product_links(product_url, current_category)
     arr_product_list = []
     document = using_nokogiri(product_url)
     document.css("div.listComic ul.list li").each do |item|
@@ -63,15 +63,73 @@ class Beeng
     end
 
     arr_product_list.each do |item|
-      get_chapter_detail(item)
+      get_chapter_detail(item, current_category)
     end
   end
 
   # collect chapter
-  def get_chapter_detail(chapter_url)
+  def get_chapter_detail(chapter_url, current_category)
     document = using_nokogiri(chapter_url)
+    arr_chapters = []
+    title = document.css("div.detail h1").first.content
+    short_description = document.css("div.shortDetail").first.content.strip
+    current_view_counts = document.at('ul li:contains("Lượt xem")').content.squish.gsub("Lượt xem:", "").squish.gsub(",", "").to_i
+    thumbnail_url = document.at('div.cover img').attribute('src').value
+
+    document.css('div.listChapters ul.list li').each do |item|
+      begin
+        arr_chapters << item.at("a").attribute("href").value
+      rescue
+        puts "Error get chapter detail #{item}"
+      end
+    end
+    byebug
+    begin
+      current_product = current_category.products.find_by(title: title)
+      byebug
+      if current_product
+        current_product.short_description = short_description
+        current_product.current_view_counts = current_view_counts
+        current_product.thumbnail_url = thumbnail_url
+        byebug
+        current_product.save
+      else
+        byebug
+        current_product = current_category.products.create({
+          title: title,
+          short_description: short_description,
+          current_view_counts: current_view_counts,
+          thumbnail_url: thumbnail_url
+        })
+      end
+      byebug
+      arr_chapters.each do |chapter_url|
+        get_all_images_chapter(chapter_url, current_product)
+      end
+
+    rescue
+      puts "Error when import database"
+    end
   end
 
-  def save_to_db
+  def get_all_images_chapter(chapter_url, current_product)
+    document = using_nokogiri(chapter_url)
+    arr_chapters = []
+    begin
+      document.css("div.readComic div.container-fuild img").each do |item |
+        arr_chapters << item.attribute("src").value
+      end
+      save_to_db(arr_chapters, current_product)
+    rescue
+      puts "Get chapter is error #{chapter_url}"
+    end
+  end
+
+  def save_to_db(arr_chapters, current_product)
+    current_product.chapters.destroy_all
+      current_product.chapters.create({
+        title: current_product.title,
+        image_list: arr_chapters.join(",")
+      })
   end
 end
