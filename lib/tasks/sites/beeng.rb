beb793ea22847d8cb60c15a85ab72eddf8d6e03c
khanhpn <phamkhanh90@gmail.com>
2021-07-05 20:17:27 +0700

    handle create crawler to get all data from beeng.org

--- lib/tasks/sites/beeng.rb
+++ lib/tasks/sites/beeng.rb
@@ -32,9 +32,10 @@ class Beeng
     arr_category = []
     raw_home_page.css("nav div ul.navbar-nav li.nav-item ul.dropdown-menu li").each do |link|
       begin
+        puts "#{Time.zone.now} start crawling category"
         arr_category << {category_link: link.css('a').first['href'], category_name: link.content}
       rescue
-        puts "Error here: #{link}"
+        puts "Error category here: #{link}"
       end
     end
     arr_category
@@ -42,6 +43,7 @@ class Beeng
 
   # start using link category to get all products
   def collect_product_paging(category)
+    puts "#{Time.zone.now} start crawling paging category"
     get_total_paging = using_nokogiri(category[:category_link]).css("div.paging ul li.hidden-xs").first.content.to_i || 1
     current_category = Category.find_or_create_by(name: category[:category_name])
 
@@ -56,6 +58,7 @@ class Beeng
     document = using_nokogiri(product_url)
     document.css("div.listComic ul.list li").each do |item|
       begin
+        puts "#{Time.zone.now} start crawling link to chapters"
         arr_product_list << item.css("div.detail h3 a").first["href"]
       rescue
         puts "Error product detail here: #{item}"
@@ -78,23 +81,24 @@ class Beeng
 
     document.css('div.listChapters ul.list li').each do |item|
       begin
-        arr_chapters << item.at("a").attribute("href").value
+        title_chapter = item.at("a span.name span.titleComic").content.squish
+        puts "Title chapter: #{title_chapter}, link chapter: #{chapter_url}"
+        arr_chapters << {url: item.at("a").attribute("href").value, title: title_chapter}
       rescue
         puts "Error get chapter detail #{item}"
       end
     end
-    byebug
+
     begin
       current_product = current_category.products.find_by(title: title)
-      byebug
+      puts "Start import product #{title}"
       if current_product
         current_product.short_description = short_description
         current_product.current_view_counts = current_view_counts
         current_product.thumbnail_url = thumbnail_url
-        byebug
+
         current_product.save
       else
-        byebug
         current_product = current_category.products.create({
           title: title,
           short_description: short_description,
@@ -102,9 +106,8 @@ class Beeng
           thumbnail_url: thumbnail_url
         })
       end
-      byebug
-      arr_chapters.each do |chapter_url|
-        get_all_images_chapter(chapter_url, current_product)
+      arr_chapters.each do |chapter|
+        get_all_images_chapter(chapter, current_product)
       end
 
     rescue
@@ -112,24 +115,31 @@ class Beeng
     end
   end
 
-  def get_all_images_chapter(chapter_url, current_product)
-    document = using_nokogiri(chapter_url)
+  def get_all_images_chapter(chapter, current_product)
+    document = using_nokogiri(chapter[:url])
     arr_chapters = []
     begin
-      document.css("div.readComic div.container-fuild img").each do |item |
+      document.css("div.readComic div.container-fuild img").each do |item|
         arr_chapters << item.attribute("src").value
       end
-      save_to_db(arr_chapters, current_product)
+      save_to_db(arr_chapters, current_product, chapter[:title])
     rescue
-      puts "Get chapter is error #{chapter_url}"
+      puts "Get chapter is error #{chapter[:url]}"
     end
   end
 
-  def save_to_db(arr_chapters, current_product)
-    current_product.chapters.destroy_all
+  def save_to_db(arr_chapters, current_product, title_chapter)
+    current_chapter = current_product.chapters.find_by(title: title_chapter)
+    puts "Start import chapter #{title_chapter}"
+    if current_chapter
+      current_chapter.image_list = arr_chapters.join(",")
+      current_chapter.save
+    else
       current_product.chapters.create({
-        title: current_product.title,
+        title: title_chapter,
         image_list: arr_chapters.join(",")
       })
+    end
+    puts "Finish import chapter #{title_chapter}"
   end
 end
