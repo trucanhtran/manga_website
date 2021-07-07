3a9b2bbe494eed4d3121bd9608ba99ab9eacb532
trucanh <anhtructran111@gmail.com>
2021-07-07 22:38:06 +0700

    get data, show chapter details and write css for view show

--- app/controllers/chapter_controller.rb
+++ app/controllers/chapter_controller.rb
@@ -2,6 +2,11 @@ class ChapterController < ApplicationController
   def show
     @chapter = Chapter.find_by(id: params[:id])
     @product = @chapter.product
-    @chapters= @product.chapters.all
+    @chapters= @product.chapters.all.order(updated_at: :asc)
+  end
+
+  def change_chapter
+    @chapter = Chapter.find_by(id: params[:chapter_id])
+    render json: @chapter
   end
 end
